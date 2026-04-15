cb3c0a06c816277a962b256487dc5098da811774
Alex.Bachir <alex.bachir@laplateforme.io>
2026-04-15 16:12:45 +0200

    modification

--- cards/src/main/java/com/cards/repository/CardsRepository.java
+++ cards/src/main/java/com/cards/repository/CardsRepository.java
@@ -1,4 +1,12 @@
 package com.cards.repository;
 
-public interface CardsRepository {
+import com.cards.model.Cards;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CardsRepository extends CrudRepository<Cards, Long> {
+
+    List<Cards> findByCustomerId(int customerId);
+
 }
