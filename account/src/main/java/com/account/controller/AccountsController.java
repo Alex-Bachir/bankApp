89b9a7f7646e2791d3bbcab7c695d7c056ca55d1
Alex.Bachir <alex.bachir@laplateforme.io>
2026-04-15 14:47:53 +0200

    update pom and update application properties port

--- account/src/main/java/com/account/controller/AccountsController.java
+++ account/src/main/java/com/account/controller/AccountsController.java
@@ -1,4 +1,26 @@
 package com.account.controller;
 
+import com.account.model.Accounts;
+import com.account.model.Customer;
+import com.account.repository.AccountsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AccountsController {
+
+    @Autowired
+    private AccountsRepository accountsRepository;
+
+    @PostMapping("/myAccount")
+    public Accounts getAccountDetails(@RequestBody Customer customer) {
+
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+        if(accounts != null){
+            return accounts;
+        }
+        return null;
+    }
 }
--- account/src/main/java/com/account/model/Customer.java
+++ account/src/main/java/com/account/model/Customer.java
@@ -1,4 +1,26 @@
 package com.account.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+@Entity
+@Data
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "customer_id")
+    private int customerId;
+    private String name;
+    private String email;
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+    @Column(name = "create_dt")
+    private LocalDate createDt;
+
+//    public int getCustomerId() {
+//        return customerId;
+//    }
+
 }
